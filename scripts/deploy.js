687294f43582bbed3c78bacb2d281275f62fc84d
shibilpk984 <99535125+shibilpk984@users.noreply.github.com>
2024-07-02 11:18:28 +0530

    Update deploy.js
    comment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,14 @@
+// import hardhat
 const hre = require("hardhat")
-
+//converting token unit from numbers to ethers
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
-
+//
 async function main() {
-  
+  //taking deployer account
   const [deployer] = await ethers.getSigners()
+  // contracts name and symbol
   const NAME = "Forum"
   const SYMBOL = "FM"
 
@@ -17,7 +19,7 @@ async function main() {
 
   console.log(`Deployed Forum Contract at: ${forum.address}\n`)
 
-  //  Channels
+  //  Channels 
   const CHANNEL_NAMES = [
      "Tech Help",
      "Current Trends ",
@@ -31,6 +33,7 @@ async function main() {
       "Gamers Cafe",
       "Channel Price List",
     ]
+    //cost of channels(in ethers)
   const COSTS = [
      tokens(0.1),
      tokens(0.01), 
@@ -43,15 +46,15 @@ async function main() {
      tokens(0.10)
      ,tokens(0.10)
      ,tokens(0.00001)]
-
-  for (var i = 0; i < 11; i++) {
+    // creating each channels by loop through channel names arrray
+    for (var i = 0; i < 11; i++) {
     const transaction = await forum.connect(deployer).createChannel(CHANNEL_NAMES[i], COSTS[i])
     await transaction.wait()
 
-    console.log(`Created text channel #${CHANNEL_NAMES[i]}`)
+    console.log(`channel Created #${CHANNEL_NAMES[i]}`)
   }
 }
-
+// to execute main function and handle errors
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
