9b5c99979308096055e94e6e13ce7f56ef8adb42
shibilpk984 <99535125+shibilpk984@users.noreply.github.com>
2024-07-02 14:00:00 +0530

    Update Channels.js
    comment

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -1,18 +1,21 @@
+// importing react library 
 import React from 'react';
-
+// defining component 'Channels'
 const Channels = ({ provider, account, forum, channels, currentChannel, setCurrentChannel }) => {
-
+  //when user click "Add New Forums" it will redirect to send a mail to contract owner for request create new channel
+  
   const handleClick = () => {
     window.location.href = 'https://mail.google.com/mail/?view=cm&fs=1&to=shibilpk984@gmail.com';
   };
 
   
-
+ //function to handle channel selection
   const channelHandler = async (channel) => {
+    // check user has joined 
   const hasJoined = await forum.hasJoined(channel.id, account)
   if(hasJoined){
     setCurrentChannel(channel)
-
+//if user didn't joined let them to mint token by selected channnel fees
   }else{
     const signer = await provider.getSigner()
     const transaction = await forum.connect(signer).mint (channel.id , { value : channel.cost })
@@ -20,12 +23,15 @@ const Channels = ({ provider, account, forum, channels, currentChannel, setCurre
   }
 
 }
+// frontend design
   return (
     <div className="channels">
       <div className="channels__text">
-        <h2>Channels</h2>
+        
+        <h2>Channels / Forums</h2>
         <ul>
           {channels.map((channel, index) => (
+            //list channels
             <li
              key={index}
              onClick={() => channelHandler(channel)}
@@ -37,6 +43,7 @@ const Channels = ({ provider, account, forum, channels, currentChannel, setCurre
         </ul>
 
       </div>
+      
 
       <div className="add">
       <h2 onClick={handleClick} style={{ cursor: 'pointer' }}>Add New Forums</h2>
