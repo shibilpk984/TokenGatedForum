e1abbe98b25d8529fb840a0c4531c3424735eaa8
shibilpk984 <99535125+shibilpk984@users.noreply.github.com>
2024-07-02 14:23:36 +0530

    Update Messages.js
    comment'

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,3 +1,4 @@
+// importing necessary items from react and socket.io
 import { useEffect, useState, useRef } from 'react'
 import { io } from "socket.io-client"
 
@@ -5,27 +6,33 @@ import { io } from "socket.io-client"
 import person from '../assets/person.svg'
 import send from '../assets/send.png'
 
-// Socket
+//define socket connection to port 3030
 const socket = io('ws://localhost:3030')
+//define the Messages component
 
 const Messages = ({ account, messages, currentChannel }) => {
   const[message, setMessage] = useState("")
   const messageEndRef = useRef(null)
-
+ // defining function to handle sending a message
   const sendMessage = async (e) => {
+    //prevent empty submission
     e.preventDefault()
-
+    //create message object
     const messageObj = {
       channel: currentChannel.id.toString(),
       account: account,
       text: message
     }
+    //checking message not empty
+    //sent the message to the server
+
     if (message !== "")
     socket.emit('new message', messageObj)
     setMessage("")
 
 
   }
+  // function to handle scrolling to the end of the messages list
   const scrollHandler = () => {
     setTimeout(() => {
       messageEndRef.current.scrollIntoView({ behavior: 'smooth' })
@@ -35,17 +42,18 @@ const Messages = ({ account, messages, currentChannel }) => {
       scrollHandler()
     })
   
-
+  
+// filter and display message of selected channel
   return (
     <div className="text">
       <div className="messages">
       {currentChannel && messages.filter(message => message.channel === currentChannel.id.toString()).map((message, index) => (
           <div className="message" key={index}>
-            <img src={person} alt="Person" />
+            <img src={person} alt="Person" /> //profile icon for users
             <div className="message_content">
-              <h3>{message.account}</h3>
+              <h3>{message.account}</h3> //display users account address
               <p>
-                {message.text}
+                {message.text} // display message
               </p>
             </div>
           </div>
@@ -55,7 +63,7 @@ const Messages = ({ account, messages, currentChannel }) => {
     <div ref={messageEndRef} />
     </div>
 
-    <form onSubmit={sendMessage}>
+    <form onSubmit={sendMessage}> // form to input and send message
     {currentChannel && account ? (
        <input type="text"
         value={message}
@@ -67,7 +75,7 @@ const Messages = ({ account, messages, currentChannel }) => {
 
       
       <button type="submit">
-        <img src={send} alt="send message" />
+        <img src={send} alt="send message" /> //submit icon 
       </button>
    </form>
 
@@ -75,5 +83,5 @@ const Messages = ({ account, messages, currentChannel }) => {
     </div>
   );
 }
-
+//export Messages componet as the default export
 export default Messages;
